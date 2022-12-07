8e27e23e05301d1091ff2252a34d1d681c4db455
CAMPUS\b8016503 <paraicbradley821@gmail.com>
2022-12-07 15:22:42 +0000

    Initial push

---
+++ CSC8503 2022/CSC8503CoreClasses/PhysicsObject.cpp
@@ -0,0 +1,93 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include "PhysicsObject.h"
+#include "PhysicsSystem.h"
+#include "Transform.h"
+using namespace NCL;
+using namespace CSC8503;
+
+PhysicsObject::PhysicsObject(Transform* parentTransform, const CollisionVolume* parentVolume, float elastic)	{
+	transform	= parentTransform;
+	volume		= parentVolume;
+
+	inverseMass = 1.0f;
+	elasticity	= elastic;
+	friction	= 0.8f;
+}
+
+PhysicsObject::~PhysicsObject()	{
+
+}
+
+void PhysicsObject::ApplyAngularImpulse(const Vector3& force) {
+	angularVelocity += inverseInteriaTensor * force;
+}
+
+void PhysicsObject::ApplyLinearImpulse(const Vector3& force) {
+	linearVelocity += force * inverseMass;
+}
+
+void PhysicsObject::AddForce(const Vector3& addedForce) {
+	force += addedForce;
+}
+
+void PhysicsObject::AddForceAtPosition(const Vector3& addedForce, const Vector3& position) {
+	Vector3 localPos = position - transform->GetPosition();
+
+	force  += addedForce;
+	torque += Vector3::Cross(localPos, addedForce);
+}
+
+void PhysicsObject::AddTorque(const Vector3& addedTorque) {
+	torque += addedTorque;
+}
+
+void PhysicsObject::ClearForces() {
+	force				= Vector3();
+	torque				= Vector3();
+}
+
+void PhysicsObject::InitCubeInertia() {
+	Vector3 dimensions	= transform->GetScale();
+
+	Vector3 fullWidth = dimensions * 2;
+
+	Vector3 dimsSqr		= fullWidth * fullWidth;
+
+	inverseInertia.x = (12.0f * inverseMass) / (dimsSqr.y + dimsSqr.z);
+	inverseInertia.y = (12.0f * inverseMass) / (dimsSqr.x + dimsSqr.z);
+	inverseInertia.z = (12.0f * inverseMass) / (dimsSqr.x + dimsSqr.y);
+}
+
+void PhysicsObject::InitSphereInertia(bool hollow) {
+	float radius	= transform->GetScale().GetMaxElement();
+	//float i			= 2.5f * inverseMass / (radius*radius);
+	float i = hollow ? 1.5f  : 2.5f;
+	i *= inverseMass / (radius * radius);
+
+	inverseInertia	= Vector3(i, i, i);
+}
+
+void PhysicsObject::InitCapsuleInertia() {
+	float r = transform->GetScale().x / 2;
+	float h = transform->GetScale().y / 2 - r;
+
+	float mcy = h * pow(r, 2) * M_PI;
+	float mhs = (2 * pow(r, 3) * M_PI) / 3;
+	float m = mcy + 2 * mhs;
+
+	float ix = mcy * (pow(h,2) / 12 + pow(r,2) / 4) + 2 * mhs * (2 * pow(r,2) / 5 + pow(h / 2,2) + 3 * h * r / 8);
+	float iy = mcy * (pow(r, 2) / 2) + 2 * mhs * (2 * pow(r, 2) / 5);
+	float iz = mcy * (pow(h, 2) / 12 + pow(r, 2) / 4) + 2 * mhs * (2 * pow(r, 2) / 5 + pow(h / 2, 2) + 3 * h * 3 / 8);
+
+	inverseInertia = Vector3(1 / ix, 1 / iy, 1 / iz);
+}
+
+void PhysicsObject::UpdateInertiaTensor() {
+	Quaternion q = transform->GetOrientation();
+	
+	Matrix3 invOrientation	= Matrix3(q.Conjugate());
+	Matrix3 orientation		= Matrix3(q);
+
+	inverseInteriaTensor = orientation * Matrix3::Scale(inverseInertia) *invOrientation;
+}
---
+++ CSC8503 2022/CSC8503CoreClasses/PhysicsObject.h
@@ -0,0 +1,97 @@
+#pragma once
+using namespace NCL::Maths;
+
+namespace NCL {
+	class CollisionVolume;
+	
+	namespace CSC8503 {
+		class Transform;
+
+		class PhysicsObject	{
+		public:
+			PhysicsObject(Transform* parentTransform, const CollisionVolume* parentVolume, float elastic = 0.66f);
+			~PhysicsObject();
+
+			Vector3 GetLinearVelocity() const {
+				return linearVelocity;
+			}
+
+			Vector3 GetAngularVelocity() const {
+				return angularVelocity;
+			}
+
+			Vector3 GetTorque() const {
+				return torque;
+			}
+
+			Vector3 GetForce() const {
+				return force;
+			}
+
+			void SetInverseMass(float invMass) {
+				inverseMass = invMass;
+			}
+
+			float GetInverseMass() const {
+				return inverseMass;
+			}
+
+			float GetCoefficientOfRestitution() {
+				return elasticity;
+			}
+
+			void SetCoefficientOfRestitution(float e) {
+				elasticity = e;
+			}
+
+			void ApplyAngularImpulse(const Vector3& force);
+			void ApplyLinearImpulse(const Vector3& force);
+			
+			void AddForce(const Vector3& force);
+
+			void AddForceAtPosition(const Vector3& force, const Vector3& position);
+
+			void AddTorque(const Vector3& torque);
+
+
+			void ClearForces();
+
+			void SetLinearVelocity(const Vector3& v) {
+				linearVelocity = v;
+			}
+
+			void SetAngularVelocity(const Vector3& v) {
+				angularVelocity = v;
+			}
+
+			void InitCubeInertia();
+			void InitSphereInertia(bool hollow = false);
+			void InitCapsuleInertia();
+
+			void UpdateInertiaTensor();
+
+			Matrix3 GetInertiaTensor() const {
+				return inverseInteriaTensor;
+			}
+
+		protected:
+			const CollisionVolume* volume;
+			Transform*		transform;
+
+			float inverseMass;
+			float elasticity;
+			float friction;
+
+			//linear stuff
+			Vector3 linearVelocity;
+			Vector3 force;
+			
+			//angular stuff
+			Vector3 angularVelocity;
+			Vector3 torque;
+			Vector3 inverseInertia;
+			Matrix3 inverseInteriaTensor;
+		};
+	}
+}
+
